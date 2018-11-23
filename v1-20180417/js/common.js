2009313e1f45bdf65b1c1a32855fe2562fabcfb0
whj <1004609378@qq.com>
2018-11-23 18:14:52 +0800

    增加v2 引导入口

--- v1-20180417/js/common.js
+++ v1-20180417/js/common.js
@@ -1,4 +1,3 @@
-
 //head
 var navmore = [
     // {
@@ -17,9 +16,9 @@ var navmore = [
     //     "href" : "https://dadiu.github.io/"
     // },
     {
-        "name" : "work",
-        "china" : "日常们",
-        "href" : "http://whj.fayfox.com/work.html"
+        "name": "work",
+        "china": "日常们",
+        "href": "http://whj.fayfox.com/work.html"
     },
     // {
     //     "name" : "ui",
@@ -42,16 +41,22 @@ var navmore = [
     //     "href" : "http://whj.fayfox.com/game.html"
     // },
     {
-        "name" : "github",
-        "china" : "github",
-        "href" : "https://github.com/dadiu",
-        "open" : "_blank"
+        "name": "github",
+        "china": "github",
+        "href": "https://github.com/dadiu",
+        "open": "_blank"
     },
     {
-        "name" : "wiki",
-        "china" : "wiki",
-        "href" : "http://whj.fayfox.com/wiki/"
-    }
+        "name": "wiki",
+        "china": "wiki",
+        "href": "http://whj.fayfox.com/wiki/"
+    },
+    {
+        "name": "",
+        "china": "v2更新中",
+        "href": "http://121.43.189.200",
+        "open": "_blank"
+    },
     // {
     //     "name" : "study",
     //     "china" : "日常学习",
@@ -66,25 +71,25 @@ var navmore = [
 
 //skill snav
 var skillSnavList = {
-    "other":{
-        "待分类":"http://whj.fayfox.com/skill.html?nav=skill"
+    "other": {
+        "待分类": "http://whj.fayfox.com/skill.html?nav=skill"
     },
-    "css2.0" : {
-        "css2.0" : "http://whj.fayfox.com/con_skill/skill_css.html?nav=skill"
+    "css2.0": {
+        "css2.0": "http://whj.fayfox.com/con_skill/skill_css.html?nav=skill"
     },
-    "css3.0" : {
-        "css3.0" : "http://whj.fayfox.com/con_skill/skill_css3.html?nav=skill"
+    "css3.0": {
+        "css3.0": "http://whj.fayfox.com/con_skill/skill_css3.html?nav=skill"
     },
-    "js" : {
-        "常用公用js" :"http://whj.fayfox.com/con_skill/skill_js.html?nav=skill",
-        "responsiveslides" :"http://whj.fayfox.com/con_skill/skill_slideshow.html?nav=skill"
+    "js": {
+        "常用公用js": "http://whj.fayfox.com/con_skill/skill_js.html?nav=skill",
+        "responsiveslides": "http://whj.fayfox.com/con_skill/skill_slideshow.html?nav=skill"
     }
 };
 
 //skill snav
 var shareSnavList = {
-    "1":{
-        "响应式布局":"http://whj.fayfox.com/share.html?nav=share"
+    "1": {
+        "响应式布局": "http://whj.fayfox.com/share.html?nav=share"
     }
 };
 
@@ -92,9 +97,9 @@ var shareSnavList = {
 //foot
 var common = {
 
-    urlObj : {},
+    urlObj: {},
 
-    init : function(){
+    init: function() {
 
         var _t = this;
         _t.Controller.urlFun();
@@ -102,96 +107,107 @@ var common = {
         _t.view.ft();
     },
 
-    view : {
+    view: {
 
         //顶部menu 
         //依赖 navmore && common.Controller.urlFun()
-        hd : function(){
+        hd: function() {
             var defaults = {
-                'logo' : '<i class=\"iconfont\">&#xe60b;</i>个吴海晶',      
-                'href' : 'http://whj.fayfox.com/',
-                'nav' : navmore
-            },
-            objs = {
-                navParent : $('<div>').addClass('nav'),
-                navChild : $('<div>').addClass('menu'),
-                wrapChild : $('<div>').addClass('wrap'),
-                dl : $('<dl>'),
-                btn : $('<b>').addClass('iconfont')
-            },
-            hdFunc = {
+                    'logo': '<i class=\"iconfont\">&#xe60b;</i>个吴海晶',
+                    'href': 'http://whj.fayfox.com/',
+                    'nav': navmore
+                },
+                objs = {
+                    navParent: $('<div>').addClass('nav'),
+                    navChild: $('<div>').addClass('menu'),
+                    wrapChild: $('<div>').addClass('wrap'),
+                    dl: $('<dl>'),
+                    btn: $('<b>').addClass('iconfont')
+                },
+                hdFunc = {
 
-                init : function(options, objs){
+                    init: function(options, objs) {
 
-                    var _t = this,
-                        DOMP = '<div class="wrap"><ul class="cb">', 
-                        DOMC = '', dd = '', i = 0, len = options.nav.length,
-                        openUrlBoo="";
-                    
-                    /**** 主菜单 ***/
-                    if(options.nav.length > 0){
-                        for(; i < len; i++){
-                            if(options.nav[i].open){
-                                openUrlBoo = "target=\""+options.nav[i].open+"\"";
-                            } else {
-                                openUrlBoo ="";
+                        var _t = this,
+                            DOMP = '<div class="wrap"><ul class="cb">',
+                            DOMC = '',
+                            dd = '',
+                            i = 0,
+                            len = options.nav.length,
+                            openUrlBoo = "",
+                            openUrl = "";
+
+                        /**** 主菜单 ***/
+                        if (options.nav.length > 0) {
+
+                            for (; i < len; i++) {
+                                if (options.nav[i].open) {
+                                    openUrlBoo = "target=\"" + options.nav[i].open + "\"";
+                                } else {
+                                    openUrlBoo = "";
+                                }
+
+                                if (name.length > 0) {
+                                    openUrl = options.nav[i].href + '?nav=' + options.nav[i].name;
+                                } else {
+                                    openUrl = options.nav[i].href
+                                }
+
+                                DOMP += '<li data-nav="' + options.nav[i].name + '">' +
+                                    '<a ' + openUrlBoo + ' href="' + openUrl + '">' +
+                                    options.nav[i].china + '</a></li>';
                             }
 
-                            DOMP += '<li data-nav="' + options.nav[i].name+ '">' + 
-                                '<a '+ openUrlBoo +' href="'+ options.nav[i].href + '?nav=' + options.nav[i].name + '">' + 
-                                options.nav[i].china + '</a></li>';
-                        }
-
-                        DOMP += '</ul>' + 
-                                '<a class="nav_cp" href="' + options.href + '">'+
-                                '<span>' + options.logo + '</span>'+
+                            DOMP += '</ul>' +
+                                '<a class="nav_cp" href="' + options.href + '">' +
+                                '<span>' + options.logo + '</span>' +
                                 //'<i class="fa fa-user"></i>'+
                                 '</a></div>';
 
-                        objs.navParent.append(DOMP);
+                            objs.navParent.append(DOMP);
 
-                        /*** 插入html ***/
-                        $("body").prepend(objs.navParent);
+                            /*** 插入html ***/
+                            $("body").prepend(objs.navParent);
 
-                        //事件
-                        _t.evenP(options, objs);
+                            //事件
+                            _t.evenP(options, objs);
+                        }
+
+                    },
+
+                    //主菜单 当前状态
+                    evenP: function(options, objs) {
+
+                        var _t = this;
+
+                        if (common.urlObj.nav) {
+                            $('.nav li[data-nav="' + common.urlObj.nav + '"]').addClass('crt').siblings().removeClass('crt');
+                        } else {
+                            $('.nav li[data-nav="home"]').addClass('crt').siblings().removeClass('crt');
+                        }
+
                     }
-
-                },
-
-                //主菜单 当前状态
-                evenP : function(options, objs){
-
-                    var _t = this;
-
-                    if(common.urlObj.nav){
-                        $('.nav li[data-nav="' + common.urlObj.nav + '"]').addClass('crt').siblings().removeClass('crt');
-                    } else {
-                        $('.nav li[data-nav="home"]').addClass('crt').siblings().removeClass('crt');
-                    }
-
-                }
-            };
+                };
 
             hdFunc.init(defaults, objs);
         },
 
         //底部ft
-        ft : function(){
+        ft: function() {
 
-            var DOM = "<section class=\"g_ft\">"+
+            var DOM = "<section class=\"g_ft\">" +
                 "<p class=\"g_ft_txt\">设计&手绘&前端 BY 7个吴海晶.  |  @ 2015-2025</p></section>";
 
             $("body").append(DOM);
         },
 
         //左菜单
-        snav : function(list){
+        snav: function(list) {
             var DOM = "";
-            $.each(list, function(k,j){
+            $.each(list, function(k, j) {
 
                 DOM += "<ul>";
-                $.each(j,function(txt,link){
+                $.each(j, function(txt, link) {
                     DOM += "<li><a href=\"" + link + "\">" + txt + "</a></li>";
                 });
                 DOM += "</ul>";
@@ -201,16 +217,16 @@ var common = {
         }
     },
 
-    Controller : {
+    Controller: {
 
         //分解url
-        urlFun : function(){
+        urlFun: function() {
             var href = window.location.search;
 
-            if(href.indexOf('?') != -1){
+            if (href.indexOf('?') != -1) {
                 var hrefArr = href.substr(1).split('&'),
                     hrefLen = hrefArr.length;
-                for(var i = 0; i < hrefLen; i++ ){
+                for (var i = 0; i < hrefLen; i++) {
                     common.urlObj[hrefArr[i].split('=')[0]] = hrefArr[i].split('=')[1];
 
                 }

c5ca8711e1bb5cf2e5d3c31c05439c4add419b43
whj <1004609378@qq.com>
2018-12-20 11:16:27 +0800

    修改v2引导连接地址

--- v1-20180417/js/common.js
+++ v1-20180417/js/common.js
@@ -54,7 +54,7 @@ var navmore = [
     {
         "name": "",
         "china": "v2更新中",
-        "href": "http://121.43.189.200",
+        "href": "http://dadadiu.cn:81",
         "open": "_blank"
     },
     // {

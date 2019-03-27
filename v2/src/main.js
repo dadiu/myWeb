092c95f77c36ca988c2e8cffca29303aad89a522
whj <1004609378@qq.com>
2019-03-27 16:44:29 +0800

    更改js依赖

--- v2/src/main.js
+++ v2/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router.js'
 
 import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
 // elememt-ui
